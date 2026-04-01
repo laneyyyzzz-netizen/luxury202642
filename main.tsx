74ad0e163fb710848af6760f25edb0c30f65c097
laneyyyzzz-netizen <laneyyyzzz@gmail.com>
2026-04-02 00:50:06 +0800

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import QuizApp from './QuizApp' // 确保这里指向的是 QuizApp
-import './index.css'
+import QuizApp from './QuizApp' 
+import './index.css' 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
